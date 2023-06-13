848d661ce93d7a9ca2e4d3d2e3976763847212a7
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-06-13 23:29:47 +0300

    adding BLL

--- src/repositories/products-db-repository.ts
+++ src/repositories/products-db-repository.ts
@@ -12,18 +12,9 @@ export const productsRepository = {
     },
     async findProductById(id: number): Promise<ProductType | null> {
         let product: ProductType | null = await productsCollection.findOne({id: id})
-        if (product) {
-            return product;
-        } else {
-            return null
-        }
-
+        return product;
     },
-    async createProduct(title: string): Promise<ProductType> {
-        const newProduct = {
-            id: +(new Date()),
-            title: title
-        }
+    async createProduct(newProduct: ProductType): Promise<ProductType> {
         const result = await productsCollection.insertOne(newProduct)
         return newProduct;
     },
--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -1,7 +1,8 @@
 import {Request, Response, Router} from 'express';
-import {productsRepository, ProductType} from '../repositories/products-db-repository';
+import {productsService} from '../domain/product-service';
 import {body, validationResult} from 'express-validator';
 import {inputValidationMiddleware} from '../middlewares/input-validation-middleware';
+import {ProductType} from '../repositories/db';
 
 
 
@@ -11,7 +12,7 @@ const titleValidation =  body('title').trim().isLength({min: 3, max: 10})
         .withMessage('Title length should be from 3 to 10 symbols') // f-я body
 
 productsRouter.get('/', async (req: Request, res: Response) => {
-    const foundProducts: ProductType[] = await productsRepository.findProducts(req.query.title?.toString());
+    const foundProducts: ProductType[] = await productsService.findProducts(req.query.title?.toString());
 
 
     res.send(foundProducts)
@@ -21,11 +22,11 @@ productsRouter.post('/',
     titleValidation,
     inputValidationMiddleware,
     async (req: Request, res: Response) => {
-        const newProduct: ProductType = await productsRepository.createProduct(req.body.title)
+        const newProduct: ProductType = await productsService.createProduct(req.body.title)
         res.sendStatus(201).send(newProduct)
     })
 productsRouter.get('/:id', async (req: Request, res: Response) => {
-    let product = await productsRepository.findProductById(+req.params.id)
+    let product = await productsService.findProductById(+req.params.id)
     if (product) {
         res.send(product)
     } else {
@@ -36,16 +37,16 @@ productsRouter.put('/:id',
     titleValidation,
     inputValidationMiddleware,
     async (req: Request, res: Response) => {
-    const isUpdated = await productsRepository.updateProduct(+req.params.id, req.body.title)
+    const isUpdated = await productsService.updateProduct(+req.params.id, req.body.title)
         if (isUpdated) {
-            const product: boolean = await productsRepository.findProductById(+req.params.id)
+            const product: boolean = await productsService.findProductById(+req.params.id)
             res.send(product)
         } else {
             res.send(404)
         }
 })
 productsRouter.delete('/:id', async (req: Request, res: Response) => {
-    const isDeleted = await productsRepository.deleteProduct(+req.params.id)
+    const isDeleted = await productsService.deleteProduct(+req.params.id)
     if (isDeleted) {
         res.send(204);
     } else {
